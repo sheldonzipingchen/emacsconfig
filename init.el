a6dd392cffc6ad6daf548112fcb000d640936c2d
Sheldon Chen <sheldon.ziping.chen@gmail.com>
2024-11-15 23:22:45 +0800

    保持最基础配置

--- init.el
+++ init.el
@@ -29,15 +29,13 @@
       scroll-preserve-screen-position 1)
 
 (setq-default tab-width 4
-	      indent-tabs-mode nil)
+	          indent-tabs-mode nil)
 
-; Set Font
+                                        ; Set Font
 (set-frame-font "FiraCode Nerd Font Propo 14" nil t)
 
 
 (setq backup-directory-alist (quote (("." . "~/.backups"))))
-
-
 
 
 (use-package package
@@ -61,18 +59,6 @@
 (use-package magit
   :ensure t)
 
-(use-package company
-  :ensure t
-  :defer t
-  :hook (after-init . global-company-mode)
-  :config
-  (setq company-tooltip-align-annotations t  ; 注释贴右侧对齐
-        company-tooltip-limit 20             ; 菜单里可选项数量
-        company-show-quick-access t          ; 显示编号（然后可以用 M-数字 快速选定某一项）
-        company-idle-delay .2                ; 延时多少秒后弹出
-        company-minimum-prefix-length 1      ; 至少几个字符符后开始补全
-        ))
-
 (use-package format-all
   :ensure t
   :defer t
@@ -81,239 +67,11 @@
   ;; 绑定一个手动格式化的快捷键
   :bind ("C-c f" . #'format-all-region-or-buffer))
 
-(use-package flycheck
-  :ensure t
-  :init
-  (setq flycheck-emacs-lisp-load-path 'inherit)
-  :config
-  (global-flycheck-mode))
-
-(add-hook 'prog-mode-hook 'display-line-numbers-mode)   ; 显示行号
-(add-hook 'prog-mode-hook 'electric-pair-mode)          ; 括号配对
-
-(use-package helm
-  :ensure t
-  :bind (("M-x" . helm-M-x)
-	 ("C-x C-f" . helm-find-files))
-  :config
-  ;; 全局启用 Helm minor mode
-  (helm-mode 1))
-
-(use-package projectile
-  :ensure t
-  :config
-  ;; 缓存到 ~/.emacs.d/.cache/ 文件夹下
-  (setq projectile-cache-file (expand-file-name ".cache/projectile.cache" user-emacs-directory))
-  ;; 全局 enable 这个 minor mode
-  (projectile-mode 1)
-  ;; 定义和它有关功能的 leader key
-  (define-key projectile-mode-map (kbd "C-c C-p") 'projectile-command-map))
-
-(use-package helm-projectile
-  :ensure t
-  :if (functionp 'helm)
-  :config
-  (helm-projectile-on))
-
-(use-package treesit
-  :when (and (fboundp 'treesit-available-p)
-             (treesit-available-p))
-  :config (setq treesit-font-lock-level 4)
-  :init
-  (setq treesit-language-source-alist
-	'((bash       . ("https://github.com/tree-sitter/tree-sitter-bash"))
-	  (c          . ("https://github.com/tree-sitter/tree-sitter-c"))
-	  (cpp        . ("https://github.com/tree-sitter/tree-sitter-cpp"))
-	  (css        . ("https://github.com/tree-sitter/tree-sitter-css"))
-	  (cmake      . ("https://github.com/uyha/tree-sitter-cmake"))
-	  (csharp     . ("https://github.com/tree-sitter/tree-sitter-c-sharp.git"))
-	  (dockerfile . ("https://github.com/camdencheek/tree-sitter-dockerfile"))
-	  (elisp      . ("https://github.com/Wilfred/tree-sitter-elisp"))
-	  (go         . ("https://github.com/tree-sitter/tree-sitter-go"))
-	  (gomod      . ("https://github.com/camdencheek/tree-sitter-go-mod.git"))
-	  (html       . ("https://github.com/tree-sitter/tree-sitter-html"))
-	  (java       . ("https://github.com/tree-sitter/tree-sitter-java.git"))
-	  (javascript . ("https://github.com/tree-sitter/tree-sitter-javascript"))
-	  (json       . ("https://github.com/tree-sitter/tree-sitter-json"))
-	  (lua        . ("https://github.com/Azganoth/tree-sitter-lua"))
-	  (make       . ("https://github.com/alemuller/tree-sitter-make"))
-	  (markdown   . ("https://github.com/MDeiml/tree-sitter-markdown" nil "tree-sitter-markdown/src"))
-	  (ocaml      . ("https://github.com/tree-sitter/tree-sitter-ocaml" nil "ocaml/src"))
-	  (org        . ("https://github.com/milisims/tree-sitter-org"))
-	  (python     . ("https://github.com/tree-sitter/tree-sitter-python"))
-	  (php        . ("https://github.com/tree-sitter/tree-sitter-php"))
-	  (typescript . ("https://github.com/tree-sitter/tree-sitter-typescript" nil "typescript/src"))
-	  (tsx        . ("https://github.com/tree-sitter/tree-sitter-typescript" nil "tsx/src"))
-	  (ruby       . ("https://github.com/tree-sitter/tree-sitter-ruby"))
-	  (rust       . ("https://github.com/tree-sitter/tree-sitter-rust"))
-	  (sql        . ("https://github.com/m-novikov/tree-sitter-sql"))
-	  (vue        . ("https://github.com/merico-dev/tree-sitter-vue"))
-	  (yaml       . ("https://github.com/ikatyang/tree-sitter-yaml"))
-	  (toml       . ("https://github.com/tree-sitter/tree-sitter-toml"))
-	  (zig        . ("https://github.com/GrayJack/tree-sitter-zig"))))
-  (add-to-list 'major-mode-remap-alist '(sh-mode         . bash-ts-mode))
-  (add-to-list 'major-mode-remap-alist '(c-mode          . c-ts-mode))
-  (add-to-list 'major-mode-remap-alist '(c++-mode        . c++-ts-mode))
-  (add-to-list 'major-mode-remap-alist '(c-or-c++-mode   . c-or-c++-ts-mode))
-  (add-to-list 'major-mode-remap-alist '(css-mode        . css-ts-mode))
-  (add-to-list 'major-mode-remap-alist '(js-mode         . js-ts-mode))
-  (add-to-list 'major-mode-remap-alist '(java-mode       . java-ts-mode))
-  (add-to-list 'major-mode-remap-alist '(js-json-mode    . json-ts-mode))
-  (add-to-list 'major-mode-remap-alist '(makefile-mode   . cmake-ts-mode))
-  (add-to-list 'major-mode-remap-alist '(python-mode     . python-ts-mode))
-  (add-to-list 'major-mode-remap-alist '(ruby-mode       . ruby-ts-mode))
-  (add-to-list 'major-mode-remap-alist '(conf-toml-mode  . toml-ts-mode))
-  (add-to-list 'auto-mode-alist '("\\(?:Dockerfile\\(?:\\..*\\)?\\|\\.[Dd]ockerfile\\)\\'" . dockerfile-ts-mode))
-  (add-to-list 'auto-mode-alist '("\\.go\\'" . go-ts-mode))
-  (add-to-list 'auto-mode-alist '("/go\\.mod\\'" . go-mod-ts-mode))
-  (add-to-list 'auto-mode-alist '("\\.rs\\'" . rust-ts-mode))
-  (add-to-list 'auto-mode-alist '("\\.ts\\'" . typescript-ts-mode))
-  (add-to-list 'auto-mode-alist '("\\.y[a]?ml\\'" . yaml-ts-mode)))
-
-
-(use-package eglot
-  :ensure t
-  :hook (prog-mode . eglot-ensure)
-  :bind ("C-c e f" . eglot-format))
-
-(defun efs/org-mode-setup ()
-  (org-indent-mode)
-  (variable-pitch-mode 1)
-  (visual-line-mode 1))
-
-(use-package org
-  :ensure t
-  :hook (org-mode . efs/org-mode-setup)
-  :config
-  (setq-default truncate-lines nil)
-  (setq org-ellipsis " ▾")
-
-  (setq org-agenda-start-with-log-mode t)
-  (setq org-log-done 'time)
-  (setq org-log-into-drawer t)
-
-  (setq org-agenda-files
-	    '("~/projects/notes/Tasks.org"
-	      "~/projects/notes/Habits.org"
-	      "~/projects/notes/Birthdays.org"))
-
-  (require 'org-habit)
-  (add-to-list 'org-modules 'org-habit)
-  (setq org-habit-graph-column 60)
-
-  (setq org-todo-keywords
-        '((sequence "TODO(t)" "NEXT(n)" "|" "DONE(d!)")
-          (sequence "BACKLOG(b)" "PLAN(p)" "READY(r)" "ACTIVE(a)" "REVIEW(v)" "WAIT(w@/!)" "HOLD(h)" "|" "COMPLETED(c)" "CANC(k@)")))
-
-  (setq org-refile-targets
-        '(("Archive.org" :maxlevel . 1)
-          ("Tasks.org" :maxlevel . 1)))
-
-  ;; Save Org buffers after refiling!
-  (advice-add 'org-refile :after 'org-save-all-org-buffers)
-
-  (setq org-tag-alist
-        '((:startgroup)
-                                        ; Put mutually exclusive tags here
-          (:endgroup)
-          ("@errand" . ?E)
-          ("@home" . ?H)
-          ("@work" . ?W)
-          ("agenda" . ?a)
-          ("planning" . ?p)
-          ("publish" . ?P)
-          ("batch" . ?b)
-          ("note" . ?n)
-          ("idea" . ?i)))
-
-  ;; Configure custom agenda views
-  (setq org-agenda-custom-commands
-        '(("d" "Dashboard"
-           ((agenda "" ((org-deadline-warning-days 7)))
-            (todo "NEXT"
-                  ((org-agenda-overriding-header "Next Tasks")))
-            (tags-todo "agenda/ACTIVE" ((org-agenda-overriding-header "Active Projects")))))
-
-          ("n" "Next Tasks"
-           ((todo "NEXT"
-                  ((org-agenda-overriding-header "Next Tasks")))))
-
-          ("W" "Work Tasks" tags-todo "+work-email")
-
-          ;; Low-effort next actions
-          ("e" tags-todo "+TODO=\"NEXT\"+Effort<15&+Effort>0"
-           ((org-agenda-overriding-header "Low Effort Tasks")
-            (org-agenda-max-todos 20)
-            (org-agenda-files org-agenda-files)))
-
-          ("w" "Workflow Status"
-           ((todo "WAIT"
-                  ((org-agenda-overriding-header "Waiting on External")
-                   (org-agenda-files org-agenda-files)))
-            (todo "REVIEW"
-                  ((org-agenda-overriding-header "In Review")
-                   (org-agenda-files org-agenda-files)))
-            (todo "PLAN"
-                  ((org-agenda-overriding-header "In Planning")
-                   (org-agenda-todo-list-sublevels nil)
-                   (org-agenda-files org-agenda-files)))
-            (todo "BACKLOG"
-                  ((org-agenda-overriding-header "Project Backlog")
-                   (org-agenda-todo-list-sublevels nil)
-                   (org-agenda-files org-agenda-files)))
-            (todo "READY"
-                  ((org-agenda-overriding-header "Ready for Work")
-                   (org-agenda-files org-agenda-files)))
-            (todo "ACTIVE"
-                  ((org-agenda-overriding-header "Active Projects")
-                   (org-agenda-files org-agenda-files)))
-            (todo "COMPLETED"
-                  ((org-agenda-overriding-header "Completed Projects")
-                   (org-agenda-files org-agenda-files)))
-            (todo "CANC"
-                  ((org-agenda-overriding-header "Cancelled Projects")
-                   (org-agenda-files org-agenda-files)))))))
-
-  (setq org-capture-templates
-        `(("t" "Tasks / Projects")
-          ("tt" "Task" entry (file+olp "~/projects/notes/Tasks.org" "Inbox")
-           "* TODO %?\n  %U\n  %a\n  %i" :empty-lines 1)
-
-          ("j" "Journal Entries")
-          ("jj" "Journal" entry
-           (file+olp+datetree "~/projects/notes/Journal.org")
-           "\n* %<%I:%M %p> - Journal :journal:\n\n%?\n\n"
-           ;; ,(dw/read-file-as-string "~/Notes/Templates/Daily.org")
-           :clock-in :clock-resume
-           :empty-lines 1)
-          ("jm" "Meeting" entry
-           (file+olp+datetree "~/projects/notes/Journal.org")
-           "* %<%I:%M %p> - %a :meetings:\n\n%?\n\n"
-           :clock-in :clock-resume
-           :empty-lines 1)
-
-          ("w" "Workflows")
-          ("we" "Checking Email" entry (file+olp+datetree "~/projects/notes/Journal.org")
-           "* Checking Email :email:\n\n%?" :clock-in :clock-resume :empty-lines 1)
-
-          ("m" "Metrics Capture")
-          ("mw" "Weight" table-line (file+headline "~/projects/notes/Metrics.org" "Weight")
-           "| %U | %^{Weight} | %^{Notes} |" :kill-buffer t)))
-
-  (define-key global-map (kbd "C-c j")
-              (lambda () (interactive) (org-capture nil "jj"))))
-
-(use-package org-bullets
-  :ensure t
-  :after org
-  :hook (org-mode . org-bullets-mode)
-  :custom
-  (org-bullets-bullet-list '("◉" "○" "●" "○" "●" "○" "●")))
-
 
 (add-to-list 'load-path "~/.emacs.d/beancount-mode")
 (require 'beancount)
 (add-to-list 'auto-mode-alist '("\\.beancount\\'" . beancount-mode))
+
 
 (custom-set-variables
  ;; custom-set-variables was added by Custom.
