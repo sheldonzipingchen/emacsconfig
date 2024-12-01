79b3154bccfcb9c8eaadcc6c136664acbf5cde63
Sheldon Chen <sheldon.ziping.chen@gmail.com>
2024-12-01 23:26:25 +0800

    更新配置文件
    添加 org mode 相关配置
    
    添加 python env 相关配置

--- init.el
+++ init.el
@@ -37,6 +37,16 @@
 
 (setq backup-directory-alist (quote (("." . "~/.backups"))))
 
+;; Setup use-package just in case everything isn't already installed
+(unless (package-installed-p 'use-package)
+  (package-refresh-contents)
+  (package-install 'use-package))
+
+;; Enable use-package
+(eval-when-compile
+  (require 'use-package))
+(setq use-package-always-ensure t)
+
 
 (use-package package
   :config
@@ -44,17 +54,21 @@
   (unless (bound-and-true-p package--initialized)
     (package-initialize)))
 
+
 (use-package exec-path-from-shell
   :ensure t
   :init (exec-path-from-shell-initialize))
 
+
 (use-package which-key
   :ensure t)
+
 
 (use-package gruvbox-theme
   :ensure t
   :config
   (load-theme 'gruvbox-light-soft t))
+
 
 (use-package format-all
   :ensure t
@@ -63,6 +77,104 @@
   :hook (prog-mode . format-all-mode)
   ;; 绑定一个手动格式化的快捷键
   :bind ("C-c f" . #'format-all-region-or-buffer))
+
+
+(use-package pyvenv
+  :ensure t
+  :config
+  (pyvenv-mode t)
+
+  ;; Set correct Python interpreter
+  (setq pyvenv-post-activate-hooks
+        (list (lambda ()
+                (setq python-shell-interpreter (concat pyvenv-virtual-env "bin/python3")))))
+  (setq pyvenv-post-deactivate-hooks
+        (list (lambda ()
+                (setq python-shell-interpreter "python3")))))
+
+
+(use-package magit
+  :ensure t)
+
+
+(use-package org
+  :pin gnu)
+
+
+;; Must do this so the agenda knows where to look for my files
+(setq org-agenda-files '("~/projects/notes"))
+
+;; When a TODO is set to a done state, record a timestamp
+(setq org-log-done 'time)
+
+;; Follow the links
+(setq org-return-follows-link t)
+
+;; Associate all org files with org mode
+(add-to-list 'auto-mode-alist '("\\.org\\'" . org-mode))
+
+;; Make the indentation look nicer
+(add-hook 'org-mode-hook 'org-indent-mode)
+
+;; Remap the change priority keys to use the UP or DOWN key
+(define-key org-mode-map (kbd "C-c <up>") 'org-priority-up)
+(define-key org-mode-map (kbd "C-c <down>") 'org-priority-down)
+
+;; Shortcuts for storing links, viewing the agenda, and starting a capture
+(define-key global-map "\C-cl" 'org-store-link)
+(define-key global-map "\C-ca" 'org-agenda)
+(define-key global-map "\C-cc" 'org-capture)
+
+;; When you want to change the level of an org item, use SMR
+(define-key org-mode-map (kbd "C-c C-g C-r") 'org-shiftmetaright)
+
+;; Hide the markers so you just see bold text as BOLD-TEXT and not *BOLD-TEXT*
+(setq org-hide-emphasis-markers t)
+
+;; Wrap the lines in org mode so that things are easier to read
+(add-hook 'org-mode-hook 'visual-line-mode)
+
+(use-package org-super-agenda
+  :ensure t)
+
+(use-package comment-tags
+  :ensure t)
+
+(setq org-capture-templates
+      '(
+        ("j" "Work Log Entry"
+         entry (file+datetree "~/projects/notes/work-log.org")
+         "* %?"
+         :empty-lines 0)
+
+        ("n" "Note"
+         entry (file+headline "~/projects/notes/notes.org" "Random Notes")
+         "** %?"
+         :empty-lines 0)
+
+        ("g" "General To-Do"
+         entry (file+headline "~/projects/notes/todos.org" "General Tasks")
+         "* TODO [#B] %?\n:Created: %T\n "
+         :empty-lines 0)
+        ))
+
+;; TODO states
+(setq org-todo-keywords
+      '((sequence "TODO(t)" "PLANNING(p)" "IN-PROGRESS(i@/!)" "VERIFYING(v!)" "BLOCKED(b@)" "|" "DONE(d!)" "OBE(o@!)" "WONT-DO(w@/!)" )
+        ))
+
+;; TODO colors
+(setq org-todo-keyword-faces
+      '(
+        ("TODO" . (:foreground "GoldenRod" :weight bold))
+        ("PLANNING" . (:foreground "DeepPink" :weight bold))
+        ("IN-PROGRESS" . (:foreground "Cyan" :weight bold))
+        ("VERIFYING" . (:foreground "DarkOrange" :weight bold))
+        ("BLOCKED" . (:foreground "Red" :weight bold))
+        ("DONE" . (:foreground "LimeGreen" :weight bold))
+        ("OBE" . (:foreground "LimeGreen" :weight bold))
+        ("WONT-DO" . (:foreground "LimeGreen" :weight bold))
+        ))
 
 
 (add-to-list 'load-path "~/.emacs.d/beancount-mode")
@@ -75,7 +187,7 @@
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
- '(package-selected-packages '(company)))
+ '(package-selected-packages '(gnu-elpa-keyring-update company)))
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
