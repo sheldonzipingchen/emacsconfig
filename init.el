14187052836f04991b02cfe9a77124a275e6efd2
Sheldon Chen <sheldon.ziping.chen@gmail.com>
2025-02-03 11:36:35 +0800

    添加 magit 和 flycheck 配置

--- init.el
+++ init.el
@@ -114,12 +114,24 @@
   (setq doom-modeline-height 25))
 
 
+;; 语法检查(flycheck)
+(use-package flycheck
+  :ensure t
+  :init (global-flycheck-mode))
+
+
+;; 版本控制(magit)
+(use-package magit
+  :ensure t
+  :bind ("C-x g" . magit-status))
+
+
 (custom-set-variables
  ;; custom-set-variables was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
- '(package-selected-packages '(gruvbox)))
+ '(package-selected-packages '(magit flycheck gruvbox)))
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
