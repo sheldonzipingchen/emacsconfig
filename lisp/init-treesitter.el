18655518389d44468eba1db2d5f831f0a2d77507
Sheldon Chen <sheldon.ziping.chen@gmail.com>
2025-06-08 12:03:12 +0800

    emacs 的 treesit 模式配置

---
+++ lisp/init-treesitter.el
@@ -0,0 +1,33 @@
+;; Tree-sitter 核心配置
+(use-package treesit
+  :ensure nil
+  :config
+  (setq treesit-font-lock-level 4)
+  (setq treesit-inspect-indicator t))
+
+;; 自动管理语法库
+(use-package treesit-auto
+  :ensure t
+  :custom
+  (treesit-auto-install 'prompt)
+  (global-treesit-auto-mode t))
+
+;; 替换默认模式
+(dolist (mapping '((python-mode . python-ts-mode)
+		   (js-mode . js-ts-mode)
+		   (c-mode . c-ts-mode)
+		   (c++-mode . c++-ts-mode)
+		   (go-mde . go-ts-mode)
+		   (bash-mode . bash-ts-mode)))
+  (add-to-list 'major-mode-remap-alist mapping))
+
+;; 启用高级功能
+(use-package ts-fold
+  :ensure t)
+
+(use-package treesit-fold
+  :ensure t)
+
+(add-hook 'treesit-mode-hook #'treesit-fold-indicators-mode)
+
+(provide 'init-treesitter)
